ffa143996f1b8738773bddc2ab332782c8357846
JakaylaDA <127620735+JakaylaDA@users.noreply.github.com>
2023-03-18 20:40:29 -0500

    Update Covid cases 1.sql

--- Covid cases 1.sql
+++ Covid cases 1.sql
@@ -1,10 +1,15 @@
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+
+
+--Selecting data that will be used to figure out when the first cases starting appearing.
+
+Select SUM(new_cases) as total_case., SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From Portfolio..CovidDeaths
 --Where location like '%states%'
 where continent is not null 
 --Group By date
 order by 1,2
 
+-- Looking at total death vs total cases to determine the percentage of people who died based on location.
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From Porfolio..CovidDeaths
@@ -15,7 +20,6 @@ Group by location
 order by TotalDeathCount desc
 
 
-
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From Porfolio..CovidDeaths
 --Where location like '%states%'
@@ -28,19 +32,7 @@ Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Ma
 From Portfolio..CovidDeaths
 --Where location like '%states%'
 Group by Location, Population, date
-order by PercentPopulationInfected desc
+order by PercentPopulationInfected asc
 
 
 
-
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From Portfolio..CovidDeaths
---Where location like '%states%'
-Group by Location, Population
-order by PercentPopulationInfected desc
-
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From Portfolio..CovidDeaths
---Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
